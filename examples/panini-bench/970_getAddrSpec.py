89fa0a254a2fe4a0dd6318ca06b75ac212c97d8b
Paul <zhufengminpaul@163.com>
2025-06-29 20:17:36 +0200

    add new benchmarks

---
+++ examples/panini-bench/970_getAddrSpec.py
@@ -0,0 +1,7 @@
+type Input = lang(r'[^<>]*<[^>]*>.*')
+type Output = lang(r'[^>]*')
+
+
+def getAddrSpec(email: Input) -> Output:
+  b = email.index('<', 0) + 1
+  return email[b:email.index('>', b)]
