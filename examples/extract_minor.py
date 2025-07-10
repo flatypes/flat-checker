967325b0d145803c2e4c146164ca1aad85ac8e06
Paul <zhufengminpaul@163.com>
2025-07-10 16:28:47 +0200

    rename example to match paper

--- examples/extract_minor.py
+++ examples/extract_minor.py
@@ -2,7 +2,7 @@ type VersionNumber = lang(r'[0-9]+\.[0-9]+\.[0-9]+')
 type Number = lang(r'[0-9]+')
 
 
-def f(s: VersionNumber) -> Number:
+def extract_minor(s: VersionNumber) -> Number:
     i1 = s.find('.') + 1
     i2 = s.find('.', i1)
     return s[i1:i2]
