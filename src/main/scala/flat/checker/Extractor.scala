c20a774540711a020253a140a116f36310eb75a7
Paul <zhufengminpaul@163.com>
2025-10-01 14:31:45 +0200

    Extractor: minor; README update

--- src/main/scala/flat/checker/Extractor.scala
+++ src/main/scala/flat/checker/Extractor.scala
@@ -25,22 +25,20 @@ class Extractor(using config: Config) extends LazyLogging:
     case True =>
     case Goal(c, _) =>
       val task = slv.create(ctx)
-      val t = slv.encodeExpr(c)(using task.consts)
-      task.slv.assertFormula(t.notTerm)
-      outputQuery(task)
+      val tc = slv.encodeExpr(c)(using task.consts)
+      outputQuery(task, tc)
       vcCounter += 1
     case HasType(e, t, _) =>
       val task = slv.create(ctx)
-      val t1 = slv.encodeExpr(TypeTest(e, t))(using task.consts)
-      task.slv.assertFormula(t1.notTerm)
-      outputQuery(task)
+      val tc = slv.encodeExpr(TypeTest(e, t))(using task.consts)
+      outputQuery(task, tc)
       vcCounter += 1
     case InferType(e, _) =>
       logger.warn(s"Ignore InferType goal: $e")
     case LAnd(vc1, vc2) => process(vc1, ctx); process(vc2, ctx)
     case LImp(h, vc) => process(vc, ctx + h)
 
-  private def outputQuery(task: slv.Task)(using input: os.Path): Unit =
+  private def outputQuery(task: slv.Task, conclusion: cvc5.Term)(using input: os.Path): Unit =
     val cmds = ListBuffer.empty[String]
     cmds += s"; Input: $input"
     cmds += "(set-logic ALL)"
@@ -48,6 +46,7 @@ class Extractor(using config: Config) extends LazyLogging:
       cmds += s"(declare-const $x ${t.getSort})"
     for assertion <- task.assertions do
       cmds += s"(assert $assertion)"
+    cmds += s"(assert ${conclusion.notTerm})"
     cmds += "(check-sat)"
     cmds += "(exit)"
 
