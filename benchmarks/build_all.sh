d8a55782ddb932a2966ae51b31ce631ee77de76d
Paul <zhufengminpaul@163.com>
2025-10-07 14:29:20 +0200

    fix build_all.sh

--- benchmarks/build_all.sh
+++ benchmarks/build_all.sh
@@ -2,5 +2,5 @@
 
 for method in "ostrich2" "z3-noodler-pos" "cvc5" "z3"; do
   echo "--> Building: $method"
-  cd "methods/$method" && ./build.sh && cd ..
+  cd "methods/$method" && ./build.sh && cd ../..
 done
